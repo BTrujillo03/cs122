20bcbafd6dfebc4779cb40b1f635f4cbf0cfcb76
BTrujillo03 <98411411+BTrujillo03@users.noreply.github.com>
2022-02-08 00:29:37 -0500

    ClassLab 2

---
+++ CS122/src/classlab/week2/ShapeDriver.java
@@ -0,0 +1,14 @@
+package classlab.week2;
+
+public class ShapeDriver {
+	
+	public void main (String [] args) {
+		
+		Rectangle r1 = new Rectangle ( "small rectangle",2,4);
+		Square s1 = new Square ("big square", 200);
+		
+		System.out.println(r1);
+		System.out.print(s1);
+	}
+
+}
